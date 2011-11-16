92d565bbf22a365225502a549f8c7f5056c04123
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-16 21:00:13 +0000

    update clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject foosahol "1.0.0-SNAPSHOT"
   :description "Game for foosaholics"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.1"]
 		 [ring/ring-jetty-adapter "0.3.9"]])
