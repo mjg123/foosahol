bf30acec97b3494b14e9106ba3d8c0e303d77649
Matthew Gilliard <matthew.2.gilliard@nokia.com>
2011-11-18 11:33:24 +0000

    sleep in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 curl http://foosaholics.herokuapp.com/results > foos.json
 git push heroku master
+sleep 3
 curl -XPOST http://foosaholics.herokuapp.com/import --data-binary @foos.json -H "content-type:application/json"
