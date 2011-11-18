2c0233f118acb6f77bd5f2fed350cf8652e48222
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-18 21:23:29 +0000

    post-migration tighten data controls

--- src/foosahol/rest.clj
+++ src/foosahol/rest.clj
@@ -44,9 +44,9 @@
 (defn merge-meta [result]
 
   (let [timestamp (get-in result [:meta :timestamp]
-			  (get-in result [:timestamp]
-				  (now)))]
-    (dissoc (assoc-in result [:meta :timestamp] timestamp) :timestamp)))
+                          (get-in result [:timestamp]
+                                  (now)))]
+    (assoc-in result [:meta :timestamp] timestamp)))
 
 (defn check-format [result]
 
@@ -59,82 +59,82 @@
              (map? (result :team2))))
    (throw+ "needs team1 and team2")
 
-;   (not (or (= #{:team1 :team2} (set (keys result)))
-;            (= #{:team1 :team2 :meta} (set (keys result)))
-;   (throw+ "only specify team1, team2 and optionally meta")
+   (not (or (= #{:team1 :team2} (set (keys result)))
+            (= #{:team1 :team2 :meta} (set (keys result)))))
+            (throw+ "only specify team1, team2 and optionally meta")
 
-   (not (contains? #{#{"black" "yellow"} #{"red" "blue"}}
-                   (into #{} [((result :team1) :colour)
-                              ((result :team2) :colour)] )))
-   (throw+ "teams must have colours of red/blue or yellow/black")
+            (not (contains? #{#{"black" "yellow"} #{"red" "blue"}}
+                            (into #{} [((result :team1) :colour)
+                                       ((result :team2) :colour)] )))
+            (throw+ "teams must have colours of red/blue or yellow/black")
 
-   (not (and (integer? ((result :team1) :score))
-             (integer? ((result :team2) :score))))
-   (throw+ "team1 and team2 must have integer scores")
+            (not (and (integer? ((result :team1) :score))
+                      (integer? ((result :team2) :score))))
+            (throw+ "team1 and team2 must have integer scores")
 
-   (not (or (nil? (result :meta)) (map? (result :meta))))
-   (throw+ "if specified, meta must be a map")
+            (not (or (nil? (result :meta)) (map? (result :meta))))
+            (throw+ "if specified, meta must be a map")
 
-   (not (some #{10} (map :score (vals result))))
-   (throw+ "one team must have scored ten goals")
+            (not (some #{10} (map :score (vals result))))
+            (throw+ "one team must have scored ten goals")
 
-   (not (<= 10 (+ ((result :team1) :score) ((result :team2) :score)) 19))
-   (throw+ "total score must be between 10 & 19 goals")
+            (not (<= 10 (+ ((result :team1) :score) ((result :team2) :score)) 19))
+            (throw+ "total score must be between 10 & 19 goals")
 
-   (not (every? true? (map string? [((result :team1) :attacker)
-                                    ((result :team1) :defender)
-                                    ((result :team2) :attacker)
-                                    ((result :team2) :defender)])))
-   (throw+ "both teams need an attacker and defender")
+            (not (every? true? (map string? [((result :team1) :attacker)
+                                             ((result :team1) :defender)
+                                             ((result :team2) :attacker)
+                                             ((result :team2) :defender)])))
+            (throw+ "both teams need an attacker and defender")
 
-   (not= 4 (count (into #{} [((result :team1) :attacker) ((result :team1) :defender)
-                             ((result :team2) :attacker) ((result :team2) :defender)])))
-   (throw+ "all players need to be distinct")
+            (not= 4 (count (into #{} [((result :team1) :attacker) ((result :team1) :defender)
+                                      ((result :team2) :attacker) ((result :team2) :defender)])))
+            (throw+ "all players need to be distinct")
 
-   :otherwise (merge-meta result)))
+            :otherwise (merge-meta result)))
 
-(defn save-result [r]
-  (do
-    (swap! results conj r)
-    r))
+   (defn save-result [r]
+     (do
+       (swap! results conj r)
+       r))
 
-(defn add-result [body cb]
-  (try+
-   (-> body
-       (safe-read-json)
-       (check-format)
-       (save-result)
-       (success cb))
-   (catch string? s
-     (error s cb))))
+   (defn add-result [body cb]
+     (try+
+      (-> body
+          (safe-read-json)
+          (check-format)
+          (save-result)
+          (success cb))
+      (catch string? s
+        (error s cb))))
 
-(defn cb [req]
-  ((req :query-params) "callback"))
+   (defn cb [req]
+     ((req :query-params) "callback"))
 
-(defn delete-result [timestamp]
-  (if (nil? timestamp)
-    (error "show me the timestamp")
-    (swap! results (partial filter #(not= timestamp (str (:timestamp %)))))))
+   (defn delete-result [timestamp]
+     (if (nil? timestamp)
+       (error "show me the timestamp")
+       (swap! results (partial filter #(not= timestamp (str (:timestamp %)))))))
 
-(defroutes foos-routes
-  (GET "/ping" [:as req] (success {:msg "ponk"}))
+   (defroutes foos-routes
+     (GET "/ping" [:as req] (success {:msg "ponk"}))
 
-  (GET  "/dev" [:as req] (json-str (assoc (dissoc req :body) :body (body req))))
-  (POST "/dev" [:as req] (json-str (assoc (dissoc req :body) :body (body req))))
+     (GET  "/dev" [:as req] (json-str (assoc (dissoc req :body) :body (body req))))
+     (POST "/dev" [:as req] (json-str (assoc (dissoc req :body) :body (body req))))
 
-  (DELETE "/results" [:as req] (delete-result ((req :query-params) "timestamp")))
+     (DELETE "/results" [:as req] (delete-result ((req :query-params) "timestamp")))
 
-  (GET  "/results" [:as req]
-        (if (= "POST" ((req :headers) "x-http-method-override"))
-          (add-result ((req :query-params) "body") (cb req))
-          (success {:results @results} (cb req))))
+     (GET  "/results" [:as req]
+           (if (= "POST" ((req :headers) "x-http-method-override"))
+             (add-result ((req :query-params) "body") (cb req))
+             (success {:results @results} (cb req))))
 
-  (POST "/results" [:as req] (add-result (body req) (cb req)))
+     (POST "/results" [:as req] (add-result (body req) (cb req)))
 
-  (PUT "/results" [:as req] (let [b (body req)]
-			      (doseq [res (:results (read-json b))]
-				(add-result (json-str res) nil))
-                              (success @results)))
+     (PUT "/results" [:as req] (let [b (body req)]
+                                 (doseq [res (:results (read-json b))]
+                                   (add-result (json-str res) nil))
+                                 (success @results)))
 
-  (route/files "/" {:root "resources/www-root"})
-  (route/not-found "404. Problem?"))
+     (route/files "/" {:root "resources/www-root"})
+     (route/not-found "404. Problem?"))
