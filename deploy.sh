1adc7e7d0a8efc92a6d57b5e2e46ece98686790e
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-20 00:13:27 +0000

    don't need this any more

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl http://foosaholics.herokuapp.com/results > foos.json
-git push heroku master
-sleep 3
-curl -XPUT http://foosaholics.herokuapp.com/results --data-binary @foos.json -H "content-type:application/json"
