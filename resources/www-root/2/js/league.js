d4e8f1b987b992d4b7d3696f215828125994e203
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-11-27 02:37:10 +0000

    a few badge-related tweeks

--- resources/www-root/2/js/league.js
+++ resources/www-root/2/js/league.js
@@ -43,24 +43,25 @@ var LEAGUE = (function(){
     },
 
     oppositeTeams = function(n1,n2,g){
-	if ( isTeam1(n1,g) && isTeam2(n2,g) ){ return true; }
-	if ( isTeam2(n1,g) && isTeam1(n2,g) ){ return true; }
-	return false;
+        if ( isTeam1(n1,g) && isTeam2(n2,g) ){ return true; }
+        if ( isTeam2(n1,g) && isTeam1(n2,g) ){ return true; }
+        return false;
     },
 
     getTeamPeople = function(p,val){
-	var max = _(p.team).pluck(val).reduce( function(m,n){ return Math.max(m,n); }, 0 );
-	return _(p.team).keys().filter( function(k){ return p.team[k][val] === max; });
+        var max = _(p.team).pluck(val).reduce( function(m,n){ return Math.max(m,n); }, 0 );
+        return _(p.team).keys().filter( function(k){ return p.team[k][val] === max; });
     },
 
     addBadges = function( players ){
 
-	var mx = _(players).pluck('P').reduce( function(m,n){ return Math.max(m,n); } );
-	_(players).chain().keys().filter( function(k){ return players[k].P === mx; })
-	    .each( function(p){ 
-		console.log(players[p]);
-		players[p].badges.push("KEENER");
-	    } );
+        var maxP = _(players).pluck('P').reduce( function(m,n){ return Math.max(m,n); } );
+        _(players).chain().keys().filter( function(k){ return players[k].P === maxP; })
+            .each( function(p){ players[p].badges.push("KEENER"); });
+
+        var minP = _(players).pluck('P').reduce( function(m,n){ return Math.min(m,n); } );
+        _(players).chain().keys().filter( function(k){ return players[k].P === minP; })
+            .each( function(p){ players[p].badges.push("SLACKER"); });
 
 
     };
@@ -89,59 +90,59 @@ var LEAGUE = (function(){
                     if ( lost(n,g) ){ r.L++; r.form.push('L'); }
 
                     r.hist.WR.push( Math.ceil( r.W*100 / r.P ) );
-		    r.hist.W.push( r.W );
-		    r.hist.L.push( r.L );
+                    r.hist.W.push( r.W );
+                    r.hist.L.push( r.L );
 
                     _(names).chain().reject( function(on){ return n===on; } ).each(function(on){
                         r.team[on] = r.team[on] || { bff:0,enemy:0,rival:0,sidekick:0,whippingboy:0,nemesis:0,partner:0,mismatch:0 };
                         if (played(on,g)){
                             r.team[on].bff += 1;
-			    if ( oppositeTeams(n,on,g) ){
-				r.team[on].rival += 1;
-				if ( won(n,g) ){
-				    r.team[on].whippingboy += 1;
-				} else {
-				    r.team[on].nemesis += 1;
-				}
-			    } else {
-				r.team[on].sidekick += 1;
-				if ( won(n,g) ){
-				    r.team[on].partner += 1;
-				} else {
-				    r.team[on].mismatch += 1;
-				}
-			    }
+                            if ( oppositeTeams(n,on,g) ){
+                                r.team[on].rival += 1;
+                                if ( won(n,g) ){
+                                    r.team[on].whippingboy += 1;
+                                } else {
+                                    r.team[on].nemesis += 1;
+                                }
+                            } else {
+                                r.team[on].sidekick += 1;
+                                if ( won(n,g) ){
+                                    r.team[on].partner += 1;
+                                } else {
+                                    r.team[on].mismatch += 1;
+                                }
+                            }
                         } else {
-			    r.team[on].enemy += 1;
-			}
+                            r.team[on].enemy += 1;
+                        }
 
                     });
                 }
             });
 
-	    // some stats only count when both players were present
-	    _(names).chain().reject( function(on){ return n===on; } ).each( function(on){
-		r.team[on].whippingboy /= r.team[on].bff;
-		r.team[on].nemesis     /= r.team[on].bff;
-		r.team[on].partner     /= r.team[on].bff;
-		r.team[on].mismatch    /= r.team[on].bff;
-	    });
+            // some stats only count when both players were present
+            _(names).chain().reject( function(on){ return n===on; } ).each( function(on){
+                r.team[on].whippingboy /= r.team[on].bff;
+                r.team[on].nemesis     /= r.team[on].bff;
+                r.team[on].partner     /= r.team[on].bff;
+                r.team[on].mismatch    /= r.team[on].bff;
+            });
 
             if ( r.P > 0 ){
                 r.WR = r.W*100 / r.P;
                 r.LR = r.L*100 / r.P;
                 r.GDPG = (r.GF - r.GA) / r.P;
 
-		_([ "bff", "enemy", "rival", "sidekick", "whippingboy", "nemesis", "partner", "mismatch" ]).each( function(s){
-		    r[s] = getTeamPeople(r,s);
-		});
+                _([ "bff", "enemy", "rival", "sidekick", "whippingboy", "nemesis", "partner", "mismatch" ]).each( function(s){
+                    r[s] = getTeamPeople(r,s);
+                });
             }
 
             players[n] = r;
         });
 
-	addBadges(players);
-	
+        addBadges(players);
+
         return players;
     };
 
@@ -178,6 +179,7 @@ var UI = (function(league, dom){
             row.appendChild( m('td', {innerHTML: person.GF}) );
             row.appendChild( m('td', {innerHTML: person.GA}) );
             row.appendChild( m('td', {innerHTML: person.GDPG.toFixed(2)}) );
+            row.appendChild( m('td', {innerHTML: person.badges.length}) );
             d('leaguetable').appendChild(row);
         });
     };
@@ -186,24 +188,24 @@ var UI = (function(league, dom){
         console.log(player);
         d('p-name').innerHTML = player.name;
 
-	var prev = Math.max(player.P-6,0),
-	makeHist = function(v,old){
-	    return m('div',{children:[m('span',{innerHTML:v}), m('span',{innerHTML:"("+old+")"})]});
-	};
+        var prev = Math.max(player.P-6,0),
+        makeHist = function(v,old){
+            return m('div',{children:[m('span',{innerHTML:v}), m('span',{innerHTML:"("+old+")"})]});
+        };
 
-	d('p-played').innerHTML = player.P;
-	d('p-won').innerHTML = player.W;
-	d('p-lost').innerHTML = player.L;
+        d('p-played').innerHTML = player.P;
+        d('p-won').innerHTML = player.W;
+        d('p-lost').innerHTML = player.L;
 
-	d('p-form').innerHTML = player.form.slice(-5).join("");
+        d('p-form').innerHTML = player.form.slice(-5).join("");
 
 
-	_([ "bff", "enemy", "rival", "sidekick", "whippingboy", "nemesis", "partner", "mismatch" ]).each( function(s){
-	    d('p-'+s).innerHTML = player[s].join("/");	    
-	});
+        _([ "bff", "enemy", "rival", "sidekick", "whippingboy", "nemesis", "partner", "mismatch" ]).each( function(s){
+            d('p-'+s).innerHTML = player[s].join("/");
+        });
 
-	dom.removeChildren(d('badgesbox'));
-	_(player.badges).each( function(b){ d('badgesbox').appendChild(m('span',{innerHTML:b})); } );
+        dom.removeChildren(d('badgesbox'));
+        _(player.badges).each( function(b){ d('badgesbox').appendChild(m('span',{innerHTML:b})); } );
 
     };
 
