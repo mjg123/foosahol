c69946ca38cdfff64c806951296db7c9aabfe67e
Matthew Gilliard <matthew.gilliard@gmail.com>
2012-01-06 08:23:16 +0000

    added error message

--- resources/www-root/2/js/lib/xhr.js
+++ resources/www-root/2/js/lib/xhr.js
@@ -9,6 +9,7 @@ var XHR = (function(){
         try { return new XMLHttpRequest(); } catch(e) {}
 	try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
         console.log("XMLHttpRequest not supported");
+	alert("Get out of here IE");
         return null;
     };
 
